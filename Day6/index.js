7752fdde9ac1def7273282b2bf45cbbe39223517
shiri <30csstudent@gmail.com>
2023-12-30 20:49:19 +0500

    first commit

---
+++ Day6/index.js
@@ -0,0 +1,47 @@
+// console.log("Hello"); 
+// var a=4;
+// var b=6;
+// var c=a+b;
+// console.log(c);
+// let a=4;
+// let b=8;
+// let c=a+b;
+// const a=3;
+// let b=4;
+//  a=5;
+//  {
+//     let b=6;
+//     let c=a+b;
+//     console.log(c);
+//  }
+// const c=a+b;
+// console.log(c);
+// function myfunc(a,b)
+// {
+//     let c=a+b;
+//     console.log(c);
+// }
+// const myfunc=(a,b)=>console.log(a+b);
+// const obj={
+//     a:4,
+//     b:5,
+//     sum:function()
+//     {
+//        return a+this.b; 
+//     }
+// }
+
+// obj.sum();
+//objets functions arrays
+// const arrnm=[1,2,3,4,5];
+// arrnm.pop();
+// const arr=arrnm.map(y=>console.log(arrnm[y]));
+document.getElementById("demo").innerText="It is my scripted modified paragraph";
+document.getElementById("demo").style="color:red";
+const btn=document.getElementById("bt1");
+btn.addEventListener("click",function()
+{
+    alert('Hello i am clicked');
+}
+)
+
